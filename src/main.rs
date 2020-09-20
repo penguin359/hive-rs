99a1ad995383e3276ab47fb8b38fc5f925daf52b
Loren M. Lang <lorenl@north-winds.org>
2020-09-20 16:46:32 -0700

    Initial parsing of hash leaf nodes

--- src/main.rs
+++ src/main.rs
@@ -50,16 +50,52 @@ mod tests {
         assert_eq!(node.class_name_length, 0);
         assert_eq!(node.key_name, "Control Panel");
     }
+
+    #[test]
+    fn test_parse_hash_leaf() {
+        let buf = vec![
+            0xf0, 0xff, 0xff, 0xff, 0x6c, 0x68, 0x01, 0x00,  //  |....lh..|
+            0xa8, 0x09, 0x00, 0x00, 0x82, 0x21, 0x8c, 0x70,  //  |.....!.p|
+            0xf8, 0xff, 0xff, 0xff, 0xe0, 0x2f, 0x00, 0x00,  //  |...../..|
+        ];
+        let result = parse_hash_leaf(&mut Cursor::new(&buf[6..]));
+        assert!(result.is_ok());
+        let node = result.unwrap();
+        assert_eq!(node.elements.len(), 1);
+        assert_eq!(node.elements[0].key_offset, 0x000009a8);
+        assert_eq!(node.elements[0].name_hash, 0x708c2182);
+        //assert_eq!(node.last_written, 0x01d44cd3660004ba);
+        //assert_eq!(node.access_bits, 0x00000000);
+        //assert_eq!(node.parent_offset, 0x00000020);
+        //assert_eq!(node.number_subkeys, 13);
+        //assert_eq!(node.number_volatile_subkeys, 0);
+        //assert_eq!(node.subkey_list_offset, 0x00010530);
+        //assert_eq!(node.volatile_subkey_list_offset, 0xffffffff);
+        //assert_eq!(node.key_value_count, 0);
+        //assert_eq!(node.key_value_offset, 0xffffffff);
+        //assert_eq!(node.key_security_offset, 0x00000318);
+        //assert_eq!(node.class_name_offset, 0xffffffff);
+        //assert_eq!(node.subkey_name_length_max, 26);
+        //assert_eq!(node.subkey_class_length_max, 0);
+        //assert_eq!(node.value_name_length_max, 0);
+        //assert_eq!(node.value_data_length_max, 0);
+        //assert_eq!(node.workvar, 0);
+        //assert_eq!(node.key_name_length, 13);
+        //assert_eq!(node.class_name_length, 0);
+        //assert_eq!(node.key_name, "Control Panel");
+    }
 }
 
-const INDEX_LEAF:   u16 = 'l' as u16 + 'i' as u16 * 256;  // (li) Subkeys list
-const FAST_LEAF:    u16 = 'l' as u16 + 'f' as u16 * 256;  // (lf) Subkeys list with name hints
-const HASH_LEAF:    u16 = 'l' as u16 + 'h' as u16 * 256;  // (lh) Subkeys list with name hashes
-const INDEX_ROOT:   u16 = 'r' as u16 + 'i' as u16 * 256;  // (ri) List of subkeys lists
+
+
+const _INDEX_LEAF:   u16 = 'l' as u16 + 'i' as u16 * 256;  // (li) Subkeys list
+const _FAST_LEAF:    u16 = 'l' as u16 + 'f' as u16 * 256;  // (lf) Subkeys list with name hints
+const _HASH_LEAF:    u16 = 'l' as u16 + 'h' as u16 * 256;  // (lh) Subkeys list with name hashes
+const _INDEX_ROOT:   u16 = 'r' as u16 + 'i' as u16 * 256;  // (ri) List of subkeys lists
 const KEY_NODE:     u16 = 'n' as u16 + 'k' as u16 * 256;  // (nk) Registry key node
-const KEY_VALUE:    u16 = 'v' as u16 + 'k' as u16 * 256;  // (vk) Registry key value
-const KEY_SECURITY: u16 = 's' as u16 + 'k' as u16 * 256;  // (sk) Security descriptor
-const BIG_DATA:     u16 = 'd' as u16 + 'b' as u16 * 256;  // (db) List of data segments
+const _KEY_VALUE:    u16 = 'v' as u16 + 'k' as u16 * 256;  // (vk) Registry key value
+const _KEY_SECURITY: u16 = 's' as u16 + 'k' as u16 * 256;  // (sk) Security descriptor
+const _BIG_DATA:     u16 = 'd' as u16 + 'b' as u16 * 256;  // (db) List of data segments
 
 struct KeyNode {
     flags: u16,
@@ -82,6 +118,29 @@ struct KeyNode {
     key_name_length: u16,
     class_name_length: u16,
     key_name: String,
+}
+
+struct NameHash {
+    key_offset: u32,
+    name_hash: u32,
+}
+
+struct HashLeaf {
+    elements: Vec<NameHash>,
+}
+
+fn parse_hash_leaf<R: Read + Seek>(source: &mut R) -> std::io::Result<HashLeaf> {
+    let count = source.read_u16::<LittleEndian>()?;
+    let mut elements = vec![];
+    for _ in 0..count {
+        elements.push(NameHash {
+            key_offset: source.read_u32::<LittleEndian>()?,
+            name_hash: source.read_u32::<LittleEndian>()?,
+        });
+    }
+    Ok(HashLeaf {
+        elements,
+    })
 }
 
 fn parse_key_node<R: Read + Seek>(source: &mut R) -> std::io::Result<KeyNode> {
@@ -131,30 +190,32 @@ fn parse_key_node<R: Read + Seek>(source: &mut R) -> std::io::Result<KeyNode> {
     })
 }
 
-fn load_cell<R: Read + Seek>(source: &mut R, size: &mut u32) {
-    let cell_size = source.read_i32::<LittleEndian>().unwrap();
+fn load_cell<R: Read + Seek>(source: &mut R, size: &mut u32) -> std::io::Result<()> {
+    let cell_size = source.read_i32::<LittleEndian>()?;
     assert!((cell_size.abs() & 0x07) == 0);
     *size -= cell_size.abs() as u32;
     let mut key = [0u8; 2];
-    source.read_exact(&mut key).unwrap();
+    source.read_exact(&mut key)?;
     //assert_eq!(&magic, b"hbin");
     if cell_size > 0 {
         println!("Cell unallocated");
-        source.seek(SeekFrom::Current(cell_size.abs() as i64 - 6));
-        return;
+        source.seek(SeekFrom::Current(cell_size.abs() as i64 - 6))?;
+        return Ok(());
     }
     println!("Cell size: {}", cell_size.abs());
     println!("Cell raw: {:?}", &key);
     println!("Cell key: {:?}", std::str::from_utf8(&key));
     let cell_magic = key[0] as u16 + key[1] as u16 * 256;
     let mut _buf = vec![0u8; cell_size.abs() as usize - 6];
-    source.read_exact(&mut _buf).unwrap();
+    source.read_exact(&mut _buf)?;
     match cell_magic {
         KEY_NODE => {
-            parse_key_node(&mut Cursor::new(_buf));
+            parse_key_node(&mut Cursor::new(_buf))?;
         },
         _ => {},
     }
+
+    Ok(())
 }
 
 fn main() {
@@ -185,10 +246,10 @@ fn main() {
     let mut magic = [0u8; 4];
     file.read_exact(&mut magic).unwrap();
     assert_eq!(&magic, b"hbin");
-    let offset = file.read_u32::<LittleEndian>().unwrap();
+    let _offset = file.read_u32::<LittleEndian>().unwrap();
     let mut size = file.read_u32::<LittleEndian>().unwrap();
     let _reserved = file.read_u64::<LittleEndian>().unwrap();
-    let timestamp = file.read_u64::<LittleEndian>().unwrap();
+    let _timestamp = file.read_u64::<LittleEndian>().unwrap();
     let _spare = file.read_u32::<LittleEndian>().unwrap();
     println!("Bin size: {}", size);
     size -= 32;
@@ -201,6 +262,6 @@ fn main() {
 //    println!("Cell raw: {:?}", &key);
 //    println!("Cell key: {:?}", std::str::from_utf8(&key));
 //    file.seek(SeekFrom::Current(cell_size.abs() as i64 - 6));
-      load_cell(&mut file, &mut size);
+      load_cell(&mut file, &mut size).unwrap();
     }
 }
